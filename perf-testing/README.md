2d4c213848f2f1c5fb08cdfe6c2b0c4275dca2e0
Tiina <tiina303@gmail.com>
2021-06-23 06:45:00 +0200

    nit to remove unused imports + prettier (#21)

--- perf-testing/README.md
+++ perf-testing/README.md
@@ -1,6 +1,7 @@
 To run the perf test
 
 Set the `POSTHOG_URL` and `API_KEY` env variables (the latter can be found in Posthog project/settings page), e.g.
+
 ```
 export POSTHOG_URL="https://posthog.click"
 export POSTHOG_URL="http://localhost:8000"
@@ -8,11 +9,15 @@ export API_KEY='ws6xnNaSGqAbY07-Q0SVJPJrhfGtpPKSecKDiBn97ps'
 ```
 
 Run k6 ([docs](https://k6.io/docs/)).
+
 ```
 k6 run k6-capture-events.js
 ```
 
 Sleep time can be customized to add more load.
+
 ```
 CE_SLEEP=0.1 k6 run k6-capture-events.js
 ```
+
+There's no verification here that Posthog got the data, consider checking insights through the Posthog UI.
--- perf-testing/k6-capture-events.js
+++ perf-testing/k6-capture-events.js
@@ -1,32 +1,31 @@
-import http from 'k6/http'
-import {check, group, sleep} from 'k6'
-import { Gauge } from 'k6/metrics'
-
-let eventsIngested = new Gauge('events_ingested')
+import http from "k6/http";
+import { check, sleep } from "k6";
 
 export let options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'],   // http errors should be less than 1%
-    http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
-    events_ingested: ['value > 0', 'value > 100']
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+    http_req_duration: ["p(95)<500"], // 95% of requests should be below 500ms
   },
   stages: [
-    { duration: '2m', target: 100 },
-    { duration: '2m', target: 100 },
-    { duration: '2m', target: 200 }, // Digital Ocean small lb connection limit is 250
-    { duration: '2m', target: 200 },
-    { duration: '5m', target: 0 }, // scale down. Recovery stage.
+    { duration: "2m", target: 100 },
+    { duration: "2m", target: 100 },
+    { duration: "2m", target: 200 }, // Digital Ocean small lb connection limit is 250
+    { duration: "2m", target: 200 },
+    { duration: "5m", target: 0 }, // scale down. Recovery stage.
   ],
-}
+};
 
 export default function () {
-  const res = http.post(`${__ENV.POSTHOG_URL}/e/`, JSON.stringify({
-    api_key: __ENV.API_KEY,
-    event: 'k6s_custom_event',
-    distinct_id: __VU
-  }));
+  const res = http.post(
+    `${__ENV.POSTHOG_URL}/e/`,
+    JSON.stringify({
+      api_key: __ENV.API_KEY,
+      event: "k6s_custom_event",
+      distinct_id: __VU,
+    })
+  );
 
-  check(res, { 'status 200': (r) => r.status === 200 })
+  check(res, { "status 200": (r) => r.status === 200 });
 
-  sleep(__ENV.CE_SLEEP || 1)
+  sleep(__ENV.CE_SLEEP || 1);
 }
