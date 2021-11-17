8094e729dca73517ff803be3415b1a3ee2150d92
Guido Iaquinti <guido@posthog.com>
2021-11-17 12:14:09 +0100

    WIP

--- ci/setup_ingestion_test.sh
+++ ci/setup_ingestion_test.sh
@@ -2,9 +2,6 @@
 #
 # This script setup PostHog in DEV mode to do ingestion testing
 #
-sleep 20
 
 WEB_POD=$(kubectl get pods -n posthog -l role=web -o jsonpath="{.items[].metadata.name}")
 kubectl exec "$WEB_POD" -n posthog -- python manage.py setup_dev --no-data
-
-sleep 20
