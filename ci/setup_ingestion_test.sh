84e7df2a1dcc972e581492fb3136a5cedadbeff5
Guido Iaquinti <guido@posthog.com>
2021-11-18 16:37:43 +0100

    e2e - DigitalOcean (install)

--- ci/setup_ingestion_test.sh
+++ ci/setup_ingestion_test.sh
@@ -5,5 +5,6 @@
 
 sleep 10 # TODO: remove this. It was added as the command below often errors with 'unable to upgrade connection: container not found ("posthog-web")'
 
-WEB_POD=$(kubectl get pods -n posthog -l role=web -o jsonpath="{.items[].metadata.name}")
+WEB_POD=$(kubectl get pods --all-namespaces -l app=posthog -l role=web -o jsonpath="{.items[].metadata.name}") # TODO: remove --all-namespaces once DO 1-click install installs PostHog in the `posthog` namespace
+
 kubectl exec "$WEB_POD" -n posthog -- python manage.py setup_dev --no-data
