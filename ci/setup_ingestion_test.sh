c172e3b562230dac00ae9b38041694a05224c1d7
Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>
2021-11-22 17:35:13 +0100

    e2e - DigitalOcean (install) (#196)

--- ci/setup_ingestion_test.sh
+++ ci/setup_ingestion_test.sh
@@ -6,4 +6,5 @@
 sleep 10 # TODO: remove this. It was added as the command below often errors with 'unable to upgrade connection: container not found ("posthog-web")'
 
 WEB_POD=$(kubectl get pods -n posthog -l role=web -o jsonpath="{.items[].metadata.name}")
+
 kubectl exec "$WEB_POD" -n posthog -- python manage.py setup_dev --no-data
