d68acdd77b63c30434f97bb06af0824f86b4fd03
Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>
2021-12-03 16:31:45 +0100

    Update scripts/clickhouse_operator_sync.sh
    Co-authored-by: James Greenhill <fuziontech@gmail.com>

--- scripts/clickhouse_operator_sync.sh
+++ scripts/clickhouse_operator_sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This tool fetch and format 'Altinity/clickhouse-operator'
+# This tool fetchs and formats 'Altinity/clickhouse-operator'
 # k8s resource definitions into our chart.
 #
 # Why do we need this? The 'clickhouse-operator' doesn't expose a Helm
