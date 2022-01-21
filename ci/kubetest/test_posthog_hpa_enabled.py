d7e4a06f1894145a7e67713aa907dcd1114e75dd
Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>
2022-01-21 14:10:08 +0100

    CI - add test for HPA enabled (#256)

---
+++ ci/kubetest/test_posthog_hpa_enabled.py
@@ -0,0 +1,21 @@
+import pytest
+
+from utils import cleanup_k8s, helm_install, wait_for_pods_to_be_ready
+
+HELM_INSTALL_CMD = """
+helm upgrade \
+    --install \
+    -f ../../ci/values/kubetest/test_posthog_hpa_enabled.yaml \
+    --timeout 30m \
+    --create-namespace \
+    --namespace posthog \
+    posthog ../../charts/posthog \
+    --wait-for-jobs \
+    --wait
+"""
+
+
+def test_helm_install(kube):
+    cleanup_k8s()
+    helm_install(HELM_INSTALL_CMD)
+    wait_for_pods_to_be_ready(kube)
