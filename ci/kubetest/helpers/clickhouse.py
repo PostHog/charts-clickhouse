a52f5ecd17116c4f8900ebe3f65e0ebfdb6caa8b
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-02-18 14:55:55 +0200

    reformat

--- ci/kubetest/helpers/clickhouse.py
+++ ci/kubetest/helpers/clickhouse.py
@@ -18,6 +18,7 @@ def get_clickhouse_cluster_service_spec(kube):
     service = next(iter(services.values()))
     return service.obj.spec
 
+
 def get_clickhouse_pod_spec(kube):
     pods = kube.get_pods(
         namespace="posthog",
