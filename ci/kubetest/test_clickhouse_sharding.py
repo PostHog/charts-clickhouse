2ad29d68dccb21f31f58d474be8c197f35737866
Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>
2022-05-03 14:10:11 +0200

    CI - remove hardcoded image (#362)

--- ci/kubetest/test_clickhouse_sharding.py
+++ ci/kubetest/test_clickhouse_sharding.py
@@ -4,15 +4,8 @@ import yaml
 from helpers.clickhouse import get_clickhouse_table_counts_on_all_nodes
 from helpers.utils import cleanup_helm, cleanup_k8s, install_chart, is_posthog_healthy, wait_for_pods_to_be_ready
 
-# :KLUDGE: There's unrelated code here that can be removed long-term once:
-#   - https://github.com/PostHog/posthog/pull/8730 is merged (custom image)
-#   - We've removed the need for CLICKHOUSE_REPLICATION under https://github.com/PostHog/posthog/issues/8652
 VALUES_WITH_SHARDING = """
 cloud: "local"
-
-image:
-  repository: macobo/posthog-test
-  sha: sha256:0148396b08058b33918ed1f38f095d7e2970774cdeb93fbb6d6fe9db781eec67
 
 env:
 - name: CLICKHOUSE_REPLICATION
