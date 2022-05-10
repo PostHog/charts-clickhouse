8b8e92c00f19d8948ac3ed73446882deae299132
Harry Waye <harry@posthog.com>
2022-05-10 10:23:29 +0100

    fix(clickhouse-operator): pin docker image version (#371)
    * fix(clickhouse-operator): pin docker image version
    
    Previously we were using the latest available image. Here we pin to the
    latest image.
    
    It looks like they version the chart and the operator image the same. It
    may be worth pinning them to the same version. However, this will cause
    the operator to downgrade in existing installs, so here I'm opting to
    try to keep the image version the same as current installs.
    
    * update snapshot;
    
    * Add comment
    
    * fix

--- scripts/clickhouse_operator_sync.sh
+++ scripts/clickhouse_operator_sync.sh
@@ -28,7 +28,12 @@ URL="https://raw.githubusercontent.com/Altinity/clickhouse-operator/${CLICKHOUSE
 #
 OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-test-clickhouse-operator}"
 METRICS_EXPORTER_NAMESPACE="${OPERATOR_NAMESPACE}"
-OPERATOR_IMAGE="${OPERATOR_IMAGE:-altinity/clickhouse-operator:latest}"
+# NOTE: we pin to 0.19.0 here which is different to the 0.16.1 manifest version.
+# Prior to pinning we were specifying latest, so to ensure that the version
+# doesn't change on existing installs we pin to latest as of writing, thereby
+# mitigating the possibility that chart will unexpectedly update, while also 
+# maintaining current functionality.
+OPERATOR_IMAGE="${OPERATOR_IMAGE:-altinity/clickhouse-operator:0.19.0}"
 METRICS_EXPORTER_IMAGE="${METRICS_EXPORTER_IMAGE:-altinity/metrics-exporter:latest}"
 
 curl -s "${URL}" | \
@@ -57,16 +62,16 @@ kubectl-slice -f "$TMP_FOLDER/clickhouse-operator.yaml" -o "${CHART_PATH}/templa
 FILES="${CHART_PATH}/templates/clickhouse-operator/*"
 for f in $FILES
 do
-    sed -i '' '1i\
+    sed -i'' '1i\
 {{- if .Values.clickhouse.enabled }}
     ' "$f"
 
-    sed -i '' '$a\
+    sed -i'' '$a\
 {{- end }}
     ' "$f"
 
-    sed -i '' 's/#namespace: posthog$/namespace: {{ .Values.clickhouse.namespace | default .Release.Namespace }}/g' "$f"
+    sed -i'' 's/#namespace: posthog$/namespace: {{ .Values.clickhouse.namespace | default .Release.Namespace }}/g' "$f"
 
-    sed -i '' 's/namespace: posthog$/namespace: {{ .Values.clickhouse.namespace | default .Release.Namespace }}/g' "$f"
+    sed -i'' 's/namespace: posthog$/namespace: {{ .Values.clickhouse.namespace | default .Release.Namespace }}/g' "$f"
 
 done
