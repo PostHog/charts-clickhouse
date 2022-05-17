5a08e917b78f9f9e0f35292181afd34110887bf4
James Greenhill <fuziontech@gmail.com>
2022-05-17 15:43:52 -0700

    bump version that we test with to 22.3.6.5-alpine

--- ci/kubetest/test_clickhouse_different_image.py
+++ ci/kubetest/test_clickhouse_different_image.py
@@ -3,12 +3,17 @@ import pytest
 from helpers.clickhouse import get_clickhouse_pod_spec
 from helpers.utils import cleanup_helm, cleanup_k8s, install_chart, is_posthog_healthy, wait_for_pods_to_be_ready
 
+
+# Setting a value for the tag here to 22.3.6.5-alpine
+# This tests to make sure that when specified clickhouse-operator
+# actually uses the image that you give it vs. some other tag that it
+# determines that it wants to pull.
 VALUES_WITH_DIFFERENT_CLICKHOUSE_IMAGE = """
 cloud: "local"
 
 clickhouse:
   image:
-    tag: 21.11.11.1
+    tag: 22.3.6.5-alpine 
 """
 
 
