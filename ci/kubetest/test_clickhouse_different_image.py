a1a66d4cf7fe69d9cf7feac4a1bb299b2d66352b
James Greenhill <fuziontech@gmail.com>
2022-05-17 15:45:27 -0700

    isort

--- ci/kubetest/test_clickhouse_different_image.py
+++ ci/kubetest/test_clickhouse_different_image.py
@@ -3,7 +3,6 @@ import pytest
 from helpers.clickhouse import get_clickhouse_pod_spec
 from helpers.utils import cleanup_helm, cleanup_k8s, install_chart, is_posthog_healthy, wait_for_pods_to_be_ready
 
-
 # Setting a value for the tag here to 22.3.6.5-alpine
 # This tests to make sure that when specified clickhouse-operator
 # actually uses the image that you give it vs. some other tag that it
