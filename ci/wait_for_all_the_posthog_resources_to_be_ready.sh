c31efefa358a3c33f71df189c52561a681f8ec3d
Guido Iaquinti <guido@posthog.com>
2022-05-19 12:21:57 +0200

    Missing semicolon

--- ci/wait_for_all_the_posthog_resources_to_be_ready.sh
+++ ci/wait_for_all_the_posthog_resources_to_be_ready.sh
@@ -3,7 +3,7 @@
 # This script waits for all PostHog resources in k8s to be ready
 #
 echo "Waiting for jobs to be ready..."
-while ! kubectl -n posthog wait --for=condition=Complete jobs --all --timeout 10s > /dev/null 2>&1 do
+while ! kubectl -n posthog wait --for=condition=Complete jobs --all --timeout 10s > /dev/null 2>&1; do
     sleep 1
 done
 echo "All jobs are now ready!"
@@ -11,7 +11,7 @@ echo "All jobs are now ready!"
 echo "Waiting for all deployments to be ready..."
 for deployment in $(kubectl -n posthog get deployments --no-headers -o custom-columns=NAME:.metadata.name)
 do
-    while ! kubectl -n posthog rollout status deployment "$deployment" > /dev/null 2>&1 do
+    while ! kubectl -n posthog rollout status deployment "$deployment" > /dev/null 2>&1; do
         sleep 1
     done
 done
