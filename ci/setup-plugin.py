b02793431bd33a5a86da4761582cf1f17b87414b
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-06-14 15:49:34 +0300

    Debug setup

---
+++ ci/setup-plugin.py
@@ -0,0 +1,23 @@
+organization = Organization.objects.first()
+team = organization.teams.first()
+plugin = Plugin.objects.create(organization=organization, name="e2e test plugin", plugin_type="source")
+plugin_config = PluginConfig.objects.create(plugin=plugin, team=team, order=1, config={})
+
+PluginSourceFile.objects.update_or_create(
+    plugin=plugin, filename="plugin.json", source='{ "name": "e2e test plugin", "config": [] }',
+)
+PluginSourceFile.objects.update_or_create(
+    plugin=plugin, filename="index.ts", source="""
+        export async function onEvent(event, meta) {
+            const ratelimit = await meta.cache.get('ratelimit')
+            if (!ratelimit && event.event !== '$pluginEvent') {
+                posthog.capture('$pluginEvent', { event: event.event })
+                await meta.cache.set('ratelimit', 1)
+                await meta.cache.expire('ratelimit', 60)
+            }
+        }
+    """,
+)
+
+plugin_config.enabled = True
+plugin_config.save()
--- ci/setup_ingestion_test.sh
+++ ci/setup_ingestion_test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+set -ex
+
 #
 # This script setup PostHog in DEV mode to do ingestion testing
 #
@@ -10,30 +12,5 @@ WEB_POD=$(kubectl get pods -n posthog -l role=web -o jsonpath="{.items[].metadat
 kubectl exec "$WEB_POD" -n posthog -- python manage.py setup_dev --no-data # --create-e2e-test-plugin
 
 # :KLUDGE: Inline this setup script until 1.37.0 is out and `setup_dev --create-e2e-test-plugin` does something.
-echo << EOF
-from django.utils.timezone import now
-
-organization = Organization.objects.first()
-team = organization.teams.first()
-plugin = Plugin.objects.create(organization=organization, name="e2e test plugin", plugin_type="source")
-plugin_config = PluginConfig.objects.create(plugin=plugin, team=team, order=1, config={})
-
-PluginSourceFile.objects.update_or_create(
-    plugin=plugin, filename="plugin.json", source='{ "name": "e2e test plugin", "config": [] }',
-)
-PluginSourceFile.objects.update_or_create(
-    plugin=plugin, filename="index.ts", source="""
-        export async function onEvent(event, meta) {
-            const ratelimit = await meta.cache.get('ratelimit')
-            if (!ratelimit && event.event !== '$pluginEvent') {
-                posthog.capture('$pluginEvent', { event: event.event })
-                await meta.cache.set('ratelimit', 1)
-                await meta.cache.expire('ratelimit', 60)
-            }
-        }
-    """,
-)
-
-plugin_config.enabled = True
-plugin_config.save()
-EOF | kubectl exec "$WEB_POD" -n posthog -- python manage.py shell
+cat ci/setup-plugin.py | kubectl exec "$WEB_POD" -n posthog -- python manage.py shell_plus
+echo 'Setup done'
