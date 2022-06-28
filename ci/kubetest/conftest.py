6ff5fafdc321317f8157ed77402e81023c8217a4
Harry Waye <harry@posthog.com>
2022-06-28 17:40:50 +0000

    use session scope

--- ci/kubetest/conftest.py
+++ ci/kubetest/conftest.py
@@ -7,7 +7,7 @@ def kubeconfig() -> str:
     return '~/.kube/config'
 
 
-@pytest.fixture(scope="package", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def delete_everything() -> str:
     """
     Make sure we reset k8s before we start tests, ensuring we remove all things
