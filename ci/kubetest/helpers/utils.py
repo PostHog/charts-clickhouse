d5bdb03f81b910f82723cb84839e92de20a3d5b4
Harry Waye <harry@posthog.com>
2022-07-04 11:44:40 +0000

    ci(kubetest): remove unnecessary calls to the k8s API
    We are getting calls failing when calling the `status()` method of the
    kubetest API, due to either intermittent failures or pods disappearing.
    We don't need the restart anyhow as we have all the info we need from
    `get_pods`.

--- ci/kubetest/helpers/utils.py
+++ ci/kubetest/helpers/utils.py
@@ -100,7 +100,7 @@ def wait_for_pods_to_be_ready(kube, labels=None, expected_count=None, namespace=
         for pod in pods.values():
             if pod.obj.metadata.labels.get("app") == "posthog":
                 # Only ever expect things we have control over to not restart
-                assert pod.get_restart_count() == 0, f"Detected restart in pod {pod.obj.metadata.name}"
+                assert get_pod_restart_count(pod) == 0, f"Detected restart in pod {pod.obj.metadata.name}"
 
         # Note we assume that if "job-name" is a label then it is a job pod.
         non_job_pods = [pod for pod in pods.values() if "job-name" not in pod.obj.metadata.labels]
@@ -109,7 +109,7 @@ def wait_for_pods_to_be_ready(kube, labels=None, expected_count=None, namespace=
         for job_pod in job_pods:
             assert job_pod.status().phase != "Failed", f"Detected failed job {job_pod.obj.metadata.name}"
 
-        if len(pods) > 0 and all(pod.is_ready() for pod in non_job_pods):
+        if len(pods) > 0 and all(is_pod_ready(pod) for pod in non_job_pods):
             # If all non-job pods are ready, we should be ready to proceed.
             break
 
@@ -117,6 +117,47 @@ def wait_for_pods_to_be_ready(kube, labels=None, expected_count=None, namespace=
     else:
         pytest.fail("❌ Timeout raised while waiting for pods to be ready")
     log.debug("✅ Done!")
+
+
+def is_pod_ready(pod):
+    """
+    Check the referenced pod is ready, without refreshing the status from the
+    k8s API.
+
+    Using the kubetest `is_ready` we end up calling the API each time, where as
+    we already have the info we want after calling `kube.get_pods`. The
+    additional calls occasionally result in API errors so we want to reduce the
+    chance for failure and therefore test flakiness.
+    """
+    for cond in pod.obj.status.conditions:
+        # we only care about the 'ready' condition
+        if cond.type.lower() != "ready":
+            continue
+
+        # check that the readiness condition is true
+        return cond.status.lower() == "true"
+
+    # Catchall
+    return False
+
+
+def get_pod_restart_count(self) -> int:
+    """
+    Get the total number of Container restarts for the Pod.
+
+    This is a copy of the kubetest `Pod.get_restart_count` but without the call
+    to `refresh()` such that we reduce the number of calls to the API, and
+    reduce the chance for flakiness in that call.
+    """
+    container_statuses = self.obj.status.container_statuses
+    if container_statuses is None:
+        return 0
+
+    total = 0
+    for container_status in container_statuses:
+        total += container_status.restart_count
+
+    return total
 
 
 def is_posthog_healthy(kube):
