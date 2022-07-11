ad33dc15ce912661eb1c1fb54283fcb973da0ce2
Harry Waye <harry@posthog.com>
2022-07-11 12:21:17 +0100

    feat(pgbouncer): add Prometheus exporter as sidecar (#479)

---
+++ ci/kubetest/test_prometheus_pgbouncer_exporter.py
@@ -0,0 +1,19 @@
+import pytest
+
+from helpers.metrics import is_prometheus_exporter_healthy
+from helpers.utils import install_chart, is_posthog_healthy, wait_for_pods_to_be_ready
+
+VALUES_YAML = """
+cloud: local
+
+pgbouncer:
+    exporter:
+        enabled: true
+"""
+
+
+def test_prometheus_pgbouncer_exporter(kube):
+    install_chart(VALUES_YAML)
+    wait_for_pods_to_be_ready(kube)
+
+    is_posthog_healthy(kube)
